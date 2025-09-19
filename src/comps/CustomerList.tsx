115940cacd318aeecb7d53d470d7e46d19b7b82c
NickPetruccelli <NickPetro18@gmail.com>
2025-09-19 11:18:28 -0400

    added createCustomer functionality

--- src/comps/CustomerList.tsx
+++ src/comps/CustomerList.tsx
@@ -4,7 +4,9 @@ import CustomerListNode from "./CustomerListNode";
 
 function CustomerList() {
   const [customers, setCustomers] = useState<Customer[] | null>(null);
-  useEffect(() => {
+  useEffect(updateCustomerList, []);
+
+  function updateCustomerList() {
     fetch("http://localhost:8080/get-customers")
       .then((response) => response.json())
       .then((data: Customer[]) => {
@@ -14,7 +16,25 @@ function CustomerList() {
       .catch((err) => {
         console.log(err.message);
       });
-  }, []);
+  }
+
+  function createCustomer(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+    const requestOptions = { method: "PUT" };
+    fetch("http://localhost:8080/create-customer-person/" + formJson["nameInput"] + "/" + formJson["streetNumberInput"] + "/" + formJson["postalCodeInput"], requestOptions)
+      .then((response) => response.json())
+      .then((data: Customer) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      })
+      .then(() => updateCustomerList());
+  }
 
   return (
     <>
@@ -30,6 +50,24 @@ function CustomerList() {
           {customers != null ? (customers.map((customer: Customer) => {
             return <CustomerListNode customer={customer} />
           })) : ("loading...")}
+        </div>
+        <div className="createNewCustomerForm">
+          <h2>Create New Customer</h2>
+          <form onSubmit={createCustomer}>
+            <label>
+              Name: <input name="nameInput" />
+            </label>
+            <label>
+              Street Number: <input name="streetNumberInput" />
+            </label>
+            <label>
+              Postal Code: <input name="postalCodeInput" />
+            </label>
+            <label>
+              Company: <input type="checkbox" name="isCompanyInput" />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
         </div>
       </div>
     </>
